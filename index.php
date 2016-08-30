4ba7cadd4803b8038c66772a1f6464ed8ebdb365
Redfish <lesonduko@gmail.com>
2016-08-30 14:31:46 +0200

    challenge php mail

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>challenge mail </title>
+</head>
+<body>
+
+<h1>Voici mon challenge</h1>
+<a href="https://github.com/JeanSairien/php_mail">Clique ici pour le lien de mon repos xd ;-)</a>
+
+
+</body>
+</html>
